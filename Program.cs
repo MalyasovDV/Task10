b41da8c75f45c0ee84be4ad5a3f85ab1a0573156
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-15 07:07:44 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int SecondDigit(int num)
+{
+    int secdig;
+    secdig = (num % 100) / 10;
+    return secdig;
+}
+
+int num = 456;
+Console.WriteLine("Второе число числа " + num + ": " + SecondDigit(num));
+
+num = 782;
+Console.WriteLine("Второе число числа " + num + ": " + SecondDigit(num));
+
+num = 918;
+Console.WriteLine("Второе число числа " + num + ": " + SecondDigit(num));
